316d4592551071c6931893eec8e1e4846cf3ccd0
WorldObservationLog <wolc@duck.com>
2023-07-25 02:18:29 +0800

    Add new colors

--- src/mappings.py
+++ src/mappings.py
@@ -4,27 +4,35 @@ from PIL import ImageColor
 
 class ColorMapper:
     COLOR_MAP = {
+        "#6D001A": 0,  # burgundy
         "#BE0039": 1,  # dark red
         "#FF4500": 2,  # red
         "#FFA800": 3,  # orange T
-        "#FFD635": 4,  # yellow T
+        "#FFD635": 4,  # yellow
+        "#FFF8B8": 5,  # pale yellow
         "#00A368": 6,  # dark green
         "#00CC78": 7,  # green
         "#7EED56": 8,  # light green
         "#00756F": 9,  # dark teal
         "#009EAA": 10,  # teal
+        "#00CCC0": 11,  # pale teal
         "#2450A4": 12,  # dark blue
         "#3690EA": 13,  # blue T
         "#51E9F4": 14,  # light blue
         "#493AC1": 15,  # indigo
         "#6A5CFF": 16,  # periwinkle
+        "#94B3FF": 17,  # lavender
         "#811E9F": 18,  # dark purple
         "#B44AC0": 19,  # purple T
+        "#E4ABFF": 20,  # pale purple
+        "#DE10E3": 21,  # magenta
         "#FF3881": 22,  # pink
         "#FF99AA": 23,  # light pink
         "#6D482F": 24,  # dark brown
         "#9C6926": 25,  # brown
+        "#FFB470": 26,  # beige
         "#000000": 27,  # black T
+        "#515252": 28,  # dark gray
         "#898D90": 29,  # grey
         "#D4D7D9": 30,  # light grey
         "#FFFFFF": 31,  # white, T
@@ -32,27 +40,35 @@ class ColorMapper:
 
     # map of pixel color ids to verbose name (for debugging)
     NAME_MAP = {
+        0: "burgundy",
         1: "Dark Red",
         2: "Red",
         3: "Orange",
         4: "Yellow",
+        5: "Pale Yellow",
         6: "Dark Green",
         7: "Green",
         8: "Light Green",
         9: "Dark Teal",
         10: "Teal",
+        11: "Pale Teal",
         12: "Dark Blue",
         13: "Blue",
         14: "Light Blue",
         15: "Indigo",
         16: "Periwinkle",
+        17: "Lavender",
         18: "Dark Purple",
         19: "Purple",
+        20: "Pale Purple",
+        21: "Magenta",
         22: "Pink",
         23: "Light Pink",
         24: "Dark Brown",
         25: "Brown",
+        26: "Beige",
         27: "Black",
+        28: "Dark Gray",
         29: "Grey",
         30: "Light Grey",
         31: "White",
